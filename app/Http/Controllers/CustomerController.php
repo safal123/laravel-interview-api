3f86765a35c1166c287869f1dc95082d1e93cd86
Safal Pokharel <pokharelsafal66@gmail.com>
2024-10-11 01:41:57 +1100

    fix CustomerController index and added route for me route

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,26 +2,38 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreCustomerRequest;
 use App\Http\Resources\CustomerResource;
 use App\Models\Customer;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Support\Facades\Gate;
 
 class CustomerController extends Controller
 {
-    public function index(): AnonymousResourceCollection
+    public function index()
     {
         // Check if the user is authorized to view any customers
         Gate::authorize('viewAny', Customer::class);
 
-        // query
-        $query = Customer::query();
+        // Build the query
+        $query = Customer::query()
+            ->orderBy('created_at', 'desc');
         /*
          * Note: This is a simple example, in a real-world application we can
          *      use third party packages like Spatie Query Builder
          */
+
+        // check if the search query parameter is present
+        if (request()->has('search')) {
+            $search = request()->query('search');
+            $query
+                ->where('name', 'like', "%$search%")
+                ->orWhere('description', 'like', "%$search%");
+        }
+
+        if (request()->has('category')) {
+            $category = request()->query('category');
+            $query->where('category', $category);
+        }
+
         // Check if the include=contacts query parameter is present
         if (request()->has('include') && request()->query('include') === 'contacts') {
             $query
@@ -29,24 +41,52 @@ class CustomerController extends Controller
                 ->withCount('contacts');
         }
 
-        return CustomerResource::collection(
-            $query
-                ->orderBy('created_at', 'desc')
-                ->paginate(10)
-        );
+        return CustomerResource::collection($query->paginate(10));
     }
 
-    public function store(StoreCustomerRequest $request): CustomerResource
+    public function store(): CustomerResource
     {
         // Check if the user is authorized to create a customer
         Gate::authorize('create', Customer::class);
-        $data = $request->validated();
+
+        // Validate the request
+        $data = request()->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string|max:255',
+            // Category can be Gold, Silver, or Bronze
+            'category' => 'required|in:Gold,Silver,Bronze',
+            'reference' => 'required|string|max:255',
+            'start_date' => 'required|date',
+        ]);
+
+        // Create the customer
         $customer = Customer::create($data);
 
         return new CustomerResource($customer);
     }
 
-    public function destroy(Customer $customer): JsonResponse
+    public function update(Customer $customer): CustomerResource
+    {
+        // Check if the user is authorized to update the customer
+        Gate::authorize('update', $customer);
+
+        // Validate the request
+        $data = request()->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string|max:255',
+            // Category can be Gold, Silver, or Bronze
+            'category' => 'required|in:Gold,Silver,Bronze',
+            'reference' => 'required|string|max:255',
+            'start_date' => 'required|date',
+        ]);
+
+        // Update the customer
+        $customer->update($data);
+
+        return new CustomerResource($customer);
+    }
+
+    public function destroy(Customer $customer): \Illuminate\Http\JsonResponse
     {
         // Check if the user is authorized to delete the customer
         Gate::authorize('delete', $customer);
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,8 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\CustomerController;
-
 
 /*
  * Health check route
@@ -15,13 +13,16 @@ Route::get('/health', function () {
 
 Route::middleware('auth:sanctum')
     ->group(function () {
+        Route::get('/me', function (Request $request) {
+            return $request->user();
+        });
         Route::get('/customers', [CustomerController::class, 'index'])
             ->name('customers.index');
         Route::post('/customers', [CustomerController::class, 'store'])
             ->name('customers.store');
+
         Route::put('/customers/{customer}', [CustomerController::class, 'update'])
             ->name('customers.update');
         Route::delete('/customers/{customer}', [CustomerController::class, 'destroy'])
             ->name('customers.destroy');
     });
-
